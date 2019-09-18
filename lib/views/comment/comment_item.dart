be1a101b999ef5cb23e2e9f73f3d2a6d3790833f
lipengyue <pengyue.li@yi23.net>
2019-09-18 19:40:29 +0800

    【stack -》自定义按钮】

--- lib/views/comment/comment_item.dart
+++ lib/views/comment/comment_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:fullter_sina/api/user_info/userInfoModel.dart';
 import 'package:fullter_sina/utils/u_color.dart';
 import 'package:fullter_sina/models/comment_entity.dart';
+import 'dart:ui';
 
 class CommentItem extends StatefulWidget {
   CommantCommants comment;
@@ -111,32 +111,37 @@ class _CommentItemWidget extends State<CommentItem> {
     List<InlineSpan> textSpans = List();
 
     if (results.length <= 0) {
-      return [_createCommentFragmentStr(commentStr, UColor.C222222,FontWeight.w400)];
+      return [
+        _createCommentFragmentStr(commentStr, UColor.C222222, FontWeight.w400)
+      ];
     }
 
     int currentLast = 0;
     for (RegExpMatch match in results) {
       if (match.start > currentLast) {
         String noramlStr = commentStr.substring(currentLast, match.start);
-        textSpans.add(_createCommentFragmentStr(noramlStr, UColor.C222222,FontWeight.w400));
+        textSpans.add(_createCommentFragmentStr(
+            noramlStr, UColor.C222222, FontWeight.w400));
       }
 
       String matchStr = match.group(0);
-      textSpans.add(_createCommentFragmentStr(matchStr, UColor.CblueAccent,FontWeight.w500));
+      textSpans.add(_createCommentFragmentStr(
+          matchStr, UColor.CblueAccent, FontWeight.w500));
       currentLast = match.end;
     }
     if (currentLast < commentStr.length) {
       String normalStr = commentStr.substring(currentLast, commentStr.length);
-      textSpans.add(_createCommentFragmentStr(normalStr, UColor.C222222,FontWeight.w400));
+      textSpans.add(_createCommentFragmentStr(
+          normalStr, UColor.C222222, FontWeight.w400));
     }
     return textSpans;
   }
 
-  InlineSpan _createCommentFragmentStr(String str, Color color,FontWeight fontw) {
+  InlineSpan _createCommentFragmentStr(String str, Color color,
+      FontWeight fontw) {
     return TextSpan(
         text: str,
-        style:
-            TextStyle(color: color, fontSize: 13, fontWeight: fontw));
+        style: TextStyle(color: color, fontSize: 13, fontWeight: fontw));
   }
 
   _createReplayCommentInfo() {
@@ -149,9 +154,7 @@ class _CommentItemWidget extends State<CommentItem> {
         alignment: Alignment.centerRight,
         children: <Widget>[
 //          _createCommentTime(), // 时间
-          _createReplayForwardingButton(), // 转发
-          _createReplayCommentButton(), // 评论
-          _createFavorite(),
+          _createCommentButtons(),
         ],
       ),
     );
@@ -175,89 +178,102 @@ class _CommentItemWidget extends State<CommentItem> {
     );
   }
 
-  _createReplayForwardingButton() {
-    return _createButton(60, 30, Colors.black, Icons.share, () {});
-    return Positioned(
-      right: 60,
-      top: 0,
-      bottom: 0,
-//      width: 30,
-      child: IconButton(
-        tooltip: "转发按钮",
-        icon: Icon(Icons.share),
-        iconSize: 14,
-        color: UColor.CAFAFAF,
-        onPressed: () {
-          print("点击了分享按钮");
-        },
+  _createCommentButtons() {
+    double w = window.physicalSize.width;
+    return Container(
+      constraints: BoxConstraints(
+          maxWidth: 300
+      ),
+      alignment: Alignment.centerRight,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          _createReplayForwardingButton(), // 转发
+          _createReplayCommentButton(), // 评论
+          _createFavorite(),
+        ],
       ),
     );
+  }
+
+  _createReplayForwardingButton() {
+    return _createButton("", 60, 30, Colors.black, Icons.share, () {});
   }
 
   _createReplayCommentButton() {
-    return _createButton(30, 30, Colors.blue, Icons.toc, () {
+    return _createButton("", 50, 30, Colors.blue, Icons.toc, () {
       print("点击了评论");
     });
-    return Positioned(
-      right: 30,
-      top: 0,
-      bottom: 0,
-//      width: 30,
-      child: Container(
-        child: IconButton(
-          tooltip: "评论按钮",
-          icon: Icon(Icons.toc),
-          iconSize: 14,
-          color: UColor.CAFAFAF,
-          onPressed: () {
-            print("点击了评论");
-          },
-        ),
-      ),
-    );
   }
 
   _createFavorite() {
-    return _createButton(0, 30, Colors.red, Icons.favorite_border, () {
+    String str = "";
+    int count = widget.comment.status.attitudesCount;
+    if (count > 0) {
+      str = count.toString();
+    }
+
+
+    return _createButton(str, 0, 30, Colors.red, Icons.favorite_border, () {
       print("点赞");
     });
-    return Positioned(
-      right: 0,
-      top: 0,
-      bottom: 0,
-//      width: 30,
-      child: IconButton(
-        tooltip: "点赞按钮",
-        icon: Icon(Icons.favorite_border),
-        iconSize: 14,
-        color: UColor.CAFAFAF,
-        onPressed: () {
-          print("点赞");
-        },
-      ),
-    );
   }
 
-  _createButton(double right, double width, Color color, IconData icon,
+  _createButton(String rightText, double right, double width, Color color,
+      IconData icon,
       VoidCallback block) {
-    return Positioned(
-      right: right,
-      top: 0,
-      bottom: 0,
-//      width: width,
-      child: Container(
-        constraints: BoxConstraints(
-          minWidth: 30,
-        ),
-        color: color,
-        child: IconButton(
-          tooltip: "评论按钮",
-          icon: Icon(icon),
-          iconSize: 14,
-          color: UColor.CAFAFAF,
-          onPressed: block,
+    return Container(
+      height: 40,
+//      color: color,
+      child: GestureDetector(
+        onTap: block,
+        child: Stack(
+          fit: StackFit.loose,
+          children: <Widget>[
+
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 8, top: 0, bottom: 0),
+              child: Icon(
+                icon,
+                color: UColor.CAFAFAF,
+                size: 14,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              constraints: BoxConstraints(
+                minWidth: 10,
+              ),
+              padding: EdgeInsets.only(left: 20, right: 8, top: 0, bottom: 0),
+              child: Text(rightText, style: TextStyle(fontSize: 12),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+//FlatButton.icon(
+//          splashColor: Colors.white,
+//          icon: Icon(
+//            Icons.favorite_border,
+//            color: UColor.CAFAFAF,
+//            size: 14,
+//          ),
+//          label: Text(
+//            "",
+//            maxLines: 1,
+//            style: TextStyle(
+//                color: UColor.CAFAFAF,
+//                fontSize: 12,
+//                fontWeight: FontWeight.normal),
+//          ),
+//          onPressed: () {},
+//        ),
+////      ),
+//    );
